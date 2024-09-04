e313a862ac453745c540354184a5327e405bb009
ARTHUR CARVALHO ROTKIS <unifarotkis@feinet.fei.edu.br>
2024-09-03 22:18:52 -0300

    add formulas pt1

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
+from math  import * 
+
+
 class Main:
     def __init__(self):
         pass
 
+    
+
 class EnergiaEletrica:
     def __init__(self):
-        self.input = input("")
+        self.eEletrica = input("Digite a energia elétrica:")
+        self.c = 3*(10**8)
+        self.mi = (4 * 3.14) * (10 ** -7)
 
     def converterParaEnergiaMag(self):
-        pass
-
+        self.eMagnetica = self.c / self.eEletrica 
 
     def converterParaIntesidade(self):
-        pass
+        self.Intesidade = (self.eEletrica ** 2) / (2 * self.mi *self.c)
 
 class EnergiaMagnetica:
     def __init__(self):
-        self.input = input("")
+        self.eMagnetica = input("Digite a energia magnética")
+        self.c = 3*(10**8)
+        self.mi = (4 * 3.14) * (10 ** -7)
 
     def converterParaEnergiaEletrica(self):
-        pass
+        self.eEletrica = self.c * self.eMagnetica
 
     def converterParaIntesidade(self):
-        pass
+        self.Intesidade = (self.c * (self.eMagnetica ** 2) )/ (2 * self.mi)
 
 class Intensidade:
     def __init__(self):
-        self.input = input("")
+        self.intensidade = input("Digite a Intensidade")
+        self.c = 3*(10**8)
+        self.mi = (4 * 3.14) * (10 ** -7)
 
     def converterParaEnergiaEletrica(self):
-        pass
+        self.eEletrica = sqrt(2* self.mi * self.c)
 
     def converterParaEnergiaMag(self):
-        pass
+        self.eMagnetica = sqrt((2* self.mi) / self.c)
 
 class Frequencia:
     def __init__(self):
