d6999915c3ec8f126fb1894b5a2074410f54a182
ARTHUR CARVALHO ROTKIS <unifarotkis@feinet.fei.edu.br>
2024-09-03 21:56:11 -0300

    add class

---
+++ main.py
@@ -0,0 +1,89 @@
+class Main:
+    def __init__(self):
+        pass
+
+class EnergiaEletrica:
+    def __init__(self):
+        self.input = input("")
+
+    def converterParaEnergiaMag(self):
+        pass
+
+
+    def converterParaIntesidade(self):
+        pass
+
+class EnergiaMagnetica:
+    def __init__(self):
+        self.input = input("")
+
+    def converterParaEnergiaEletrica(self):
+        pass
+
+    def converterParaIntesidade(self):
+        pass
+
+class Intensidade:
+    def __init__(self):
+        self.input = input("")
+
+    def converterParaEnergiaEletrica(self):
+        pass
+
+    def converterParaEnergiaMag(self):
+        pass
+
+class Frequencia:
+    def __init__(self):
+        pass
+
+    def converterComprimentoDeOnda(self):
+        pass
+
+    def converterFrenquenciaAngular(self):
+        pass
+
+    def converterNumeroDeOnda(self):
+        pass
+
+class ComprimentoDeOnda:
+    def __init__(self):
+        pass
+
+    def converterFrenquenciaAngular(self):
+        pass
+
+    def converterFrequencia(self):
+        pass
+
+    def converterNumeroDeOnda(self):
+        pass
+
+class NumeroDeOnda:
+    def __init__(self):
+        pass
+
+    def converterFrequencia(self):
+        pass
+
+    def converterFrenquenciaAngular(self):
+        pass
+
+    def converterComprimentoDeOnda(self):
+        pass
+
+class FrequenciaAngular:
+    def __init__(self):
+        pass
+
+    def converterFrequencia(self):
+        pass
+
+    def converterComprimentoDeOnda(self):
+        pass
+
+    def converterNumeroDeOnda(self):
+        pass
+
+if __name__ == '__main__':
+    pass
